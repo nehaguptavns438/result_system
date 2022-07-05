cae52b0dd8f81669a0e32133fe921a3f7146d4c6
Neha Gupta <nehaguptavns438@gmail.com>
2022-07-06 00:33:50 +0530

    Refactoring

---
+++ resultsystem/models.py
@@ -0,0 +1,12 @@
+from . import db
+class Student(db.Model):
+    rollno = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    mobile = db.Column(db.Integer(), unique=True, nullable=False)
+    math_marks = db.Column(db.Integer())
+    science_marks = db.Column(db.Integer())
+    english_marks = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f"Student('{self.roolno}','{self.name}','{self.email}','{self.mobile}','{self.math_marks}','{self.science_marks}','{self.english_marks}')"
---
+++ run.py
@@ -0,0 +1,4 @@
+from resultsystem import app
+if __name__ =="__main__":
+    app.run(debug=True)
+
