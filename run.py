d503779b5f9d2dfa05455441115f9e3c28f88596
Neha Gupta <nehaguptavns438@gmail.com>
2022-07-24 00:49:37 +0530

    DB unresolve

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from resultsystem import create_app, db
 
 app = create_app()
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 if __name__ =="__main__":
     app.run(debug=False)
