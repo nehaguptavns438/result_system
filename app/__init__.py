9764b2e9b81ee7e4e759a74d436b2728707b7492
Neha Gupta <nehaguptavns438@gmail.com>
2022-07-27 23:45:04 +0530

    pdf

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from .routes import main
 def create_app(config_file='settings.py'):
 
     app = Flask(__name__)
+    app.config['WKHTMLTOPDF_BINARY']='wkhtmltopdf'
     
     app.config.from_pyfile(config_file)
 
