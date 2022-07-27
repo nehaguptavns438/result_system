ad4fe58d85489d6f8ea8b573c95ac0b00390c7dc
Neha Gupta <nehaguptavns438@gmail.com>
2022-07-27 12:24:37 +0530

    Revert

--- app/constants.py
+++ app/constants.py
@@ -8,7 +8,6 @@ class Admin_data:
     PASSWORD = os.environ.get('PASS')
 
 class Setting:
-    # SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
